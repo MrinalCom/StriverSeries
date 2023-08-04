48e48c1c5bf138553c9806d132d26bc9028cdb3a
Mrinal Anand <97303675+MrinalCom@users.noreply.github.com>
2023-08-04 22:16:49 +0530

    Create Pattern 5.cpp

---
+++ Pattern 5.cpp
@@ -0,0 +1,10 @@
+	void printTriangle(int n) {
+	    // code here
+	    for(int i=0;i<n;i++){
+	        for(int j=i;j<n;j++){
+	            cout<<"* ";
+	        }
+	        cout<<endl;
+	    }
+	}
+};

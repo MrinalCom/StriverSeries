8ec64ac8f25f8303241902717d874a6b042beb77
Mrinal Anand <97303675+MrinalCom@users.noreply.github.com>
2023-08-04 21:09:41 +0530

    Create Nth Fibonacci Number.cpp

---
+++ Nth Fibonacci Number.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int fibonacci(int n){
+        if(n==1 || n==2){
+                return 1;
+        
+
+        }
+        return fibonacci (n-1)+fibonacci(n-2);
+}
+
+int main()
+{
+        /*
+        *  Write your code here. 
+         *  Read input as specified in the question.
+         *  Print output as specified in the question.
+        */
+        int n;
+        cin>>n;
+        int ans=fibonacci(n);
+        cout<<ans;
+}

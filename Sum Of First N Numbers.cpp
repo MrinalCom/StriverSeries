c34b577975b2f0a3bd43950cd09056c705897c4a
Mrinal Anand <97303675+MrinalCom@users.noreply.github.com>
2023-08-17 22:29:34 +0530

    Create Sum Of First N Numbers.cpp

---
+++ Sum Of First N Numbers.cpp
@@ -0,0 +1,15 @@
+int solve(int n){
+    if(n==0){
+        return 0;
+    }
+    else
+        
+    return n*(n+1)/2;
+}
+
+
+int sumFirstN(int n) {
+    // Write Your Code Here
+    int solved=solve(n);
+    return solved;
+}

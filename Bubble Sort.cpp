25995ef80204227f6fdff4d87bd599b117340de3
Mrinal Anand <97303675+MrinalCom@users.noreply.github.com>
2023-09-02 03:08:49 +0530

    Update Bubble Sort.cpp

--- Bubble Sort.cpp
+++ Bubble Sort.cpp
@@ -1,14 +1,16 @@
 void bubbleSort(vector<int> &arr, int n) 
 {
     //write your code here
-    
+    bool didswap=0;
     for(int i=n-1;i>=0;i--){
         for(int j=0;j<=i-1;j++){
             if(arr[j]>arr[j+1]){
-               
+                didswap=1;
                 swap(arr[j],arr[j+1]);
             }
         }
-        
+    if(didswap==0){
+        break;
+    }
     }
 }

924c79da640ea43816eb34a9dff8bc9c0647cb55
Mrinal Anand <97303675+MrinalCom@users.noreply.github.com>
2023-09-03 18:27:15 +0530

    Create Delete Last Node of a Doubly Linked List.cpp

---
+++ Delete Last Node of a Doubly Linked List.cpp
@@ -0,0 +1,13 @@
+Node * deleteLastNode(Node *head) {
+    // Write your code here
+    Node*temp=head;
+    if(temp->next==nullptr){
+        return nullptr;
+    }
+    while(temp->next->next!=nullptr){
+
+        temp=temp->next;
+    }
+    temp->next=nullptr;
+    return head;
+}
